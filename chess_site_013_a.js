201162d3e7e41271fb9666cf2084ae530e41fe98
Davtonia <46623513+Davtonia@users.noreply.github.com>
2019-02-04 16:59:06 -0800

    First commit

---
+++ chess_site_013_a.js
@@ -0,0 +1,31 @@
+/*--- Avanquest WebEasy Motion Script ---*/
+
+
+function OnWeLoad()
+{
+
+	Img4={max:1,pos:0};
+	Img4[0]=new Image();Img4[0].src='button5t50.jpg';
+	Img4[1]=new Image();Img4[1].src='button5.jpg';
+	Img5={max:1,pos:0};
+	Img5[0]=new Image();Img5[0].src='button4t50.jpg';
+	Img5[1]=new Image();Img5[1].src='button4.jpg';
+	Img6={max:1,pos:0};
+	Img6[0]=new Image();Img6[0].src='button3t50.jpg';
+	Img6[1]=new Image();Img6[1].src='button3.jpg';
+	Img7={max:1,pos:0};
+	Img7[0]=new Image();Img7[0].src='button2t50.jpg';
+	Img7[1]=new Image();Img7[1].src='button2.jpg';
+	Img8={max:1,pos:0};
+	Img8[0]=new Image();Img8[0].src='button1t50.jpg';
+	Img8[1]=new Image();Img8[1].src='button1.jpg';	IDP.my=(window.parent.frames.length && parent.MyFrm)?parent.MyFrm:window;
+	IDP[1]=(V5)?'document.getElementById(\'e4\').src':(IE)?'e4.src':'document.e4.src';
+	IDP[2]=(V5)?'document.getElementById(\'e5\').src':(IE)?'e5.src':'document.e5.src';
+	IDP[3]=(V5)?'document.getElementById(\'e6\').src':(IE)?'e6.src':'document.e6.src';
+	IDP[4]=(V5)?'document.getElementById(\'e7\').src':(IE)?'e7.src':'document.e7.src';
+	IDP[5]=(V5)?'document.getElementById(\'e8\').src':(IE)?'e8.src':'document.e8.src';
+	isOvr=1;
+}
+
+
+/*--- EndOfFile ---*/
